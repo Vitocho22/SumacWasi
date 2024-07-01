8be5fb70733d90075e96efd536bcd1a9d6bded08
VSinche <115099082+Vitocho22@users.noreply.github.com>
2024-07-01 06:03:13 -0500

    Update environments.ts

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,5 +2,7 @@
 export const environment = {
   production: false,
   //endpoint: 'http://localhost:3001/'
-  endpoint: 'https://api-sumacwasi.onrender.com/'
+  //endpoint: 'https://api-sumacwasi.onrender.com/'
+  endpoint: 'https://api-sumacwasi-production.up.railway.app/'
+  
 };
